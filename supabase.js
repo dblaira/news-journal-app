7b6353962690e03f9ce1de70747468dbc593ed27
dblaira <adamdblair@gmail.com>
2025-10-31 14:12:39 -0700

    Updated subabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+// Import Supabase from CDN
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 const supabaseUrl = 'https://wqdacfrzurhpsiuvzxwo.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxZGFjZnJ6dXJocHNpdXZ6eHdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3MzQyNjcsImV4cCI6MjA3NzMxMDI2N30.IuiHf6TYw2UhB8Rk4FPTdySTg41_ndB0h1vqkJEYqmE'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxZGFjZnJ6dXJocHNpdXZ6eHdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NTkxMTYsImV4cCI6MjA2MjAzNTExNn0.IuiHf6TYw2Uh8RK4FPTdyST94l_ndB0h1vqkJEYqmE'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
