cf00a1005d2ba0dd8d16824519ca05b10245cbe3
dblaira <adamdblair@gmail.com>
2025-11-20 20:11:38 -0800

    Add SVG favicon to prevent 404 error

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📰</text></svg>" />
       </head>
       <body className={`${inter.variable} ${playfair.variable}`}>
         {children}
